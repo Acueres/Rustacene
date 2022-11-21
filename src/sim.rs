87579e1e1edf200c5c97645dc12913cf73c0bbf7
Acueres <frosteagle97@gmail.com>
2022-11-21 19:13:12 +0300

    Add basic controls

--- src/sim.rs
+++ src/sim.rs
@@ -1,5 +1,7 @@
 use bevy::core_pipeline::clear_color::ClearColorConfig;
+use bevy::utils::Duration;
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use ndarray::Array2;
 use rand::{
     distributions::{Distribution, Standard},
     Rng,
@@ -59,9 +61,9 @@ struct Coord<T> {
     pub y: T,
 }
 
-#[derive(Component)]
+#[derive(Component, Clone)]
 struct Grid {
-    pub data: Vec<Vec<bool>>,
+    pub data: Array2<bool>,
 }
 
 #[derive(Component, Clone)]
@@ -72,13 +74,25 @@ struct Parameters {
 
 struct SimTimer(Timer);
 
+#[derive(Component)]
+struct SimState {
+    pub paused: bool,
+    pub reset: bool,
+}
+
 pub struct SimPlugin;
 
 impl Plugin for SimPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(SimTimer(Timer::from_seconds(0.02, true)))
+        app.insert_resource(SimTimer(Timer::from_seconds(0.05, true)))
+            .insert_resource(SimState {
+                paused: false,
+                reset: false,
+            })
             .add_startup_system(setup)
+            .add_system(handle_input)
             .add_system(sim_step)
+            .add_system(reset_sim)
             .add_system(center_camera);
     }
 }
@@ -100,36 +114,24 @@ fn setup(
         window.set_resizable(false);
 
         let params = Parameters {
-            grid_size: 100,
-            n_particles: 20,
+            grid_size: 300,
+            n_particles: 100,
         };
-        commands.spawn().insert(params.clone());
+
+        commands.insert_resource(params.clone());
 
         let particle_width = window.width() / params.grid_size as f32;
         let particle_height = window.height() / params.grid_size as f32;
         let particle_size = 1.5 * particle_height;
 
-        let mut grid = vec![vec![false; params.grid_size]; params.grid_size];
-
-        let mut rng = rand::thread_rng();
-
-        let mut n = 0;
-        while n < params.n_particles {
-            let x = rng.gen_range(0..params.grid_size);
-            let y = rng.gen_range(0..params.grid_size);
-
-            if grid[x][y] {
-                continue;
-            }
-
-            grid[x][y] = true;
-
+        let (particles, grid) = get_particles(params.n_particles, params.grid_size);
+        for p in particles {
             commands
                 .spawn()
                 .insert(Particle)
                 .insert(Coord::<isize> {
-                    x: x as isize,
-                    y: y as isize,
+                    x: p.x as isize,
+                    y: p.y as isize,
                 })
                 .insert_bundle(MaterialMesh2dBundle {
                     mesh: meshes
@@ -143,39 +145,35 @@ fn setup(
                         .into(),
                     material: materials.add(ColorMaterial::from(Color::WHITE)),
                     transform: Transform::from_translation(Vec3::new(
-                        (x as f32) * particle_width,
-                        (y as f32) * particle_height,
+                        (p.x as f32) * particle_width,
+                        (p.y as f32) * particle_height,
                         0.,
                     )),
                     ..default()
                 });
-
-            n += 1;
         }
 
-        commands.spawn().insert(Grid { data: grid });
+        commands.insert_resource(grid);
     }
 }
 
 fn sim_step(
     time: Res<Time>,
     windows: Res<Windows>,
+    sim_state: Res<SimState>,
+    params: Res<Parameters>,
     mut timer: ResMut<SimTimer>,
-    mut particles_query: Query<(&mut Particle, &mut Coord<isize>, &mut Transform)>,
-    mut grid_query: Query<&mut Grid>,
-    params_query: Query<&Parameters>,
+    mut grid: ResMut<Grid>,
+    mut particles: Query<(&mut Particle, &mut Coord<isize>, &mut Transform)>,
 ) {
-    if timer.0.tick(time.delta()).just_finished() {
+    if !sim_state.paused && !sim_state.reset && timer.0.tick(time.delta()).just_finished() {
         for window in windows.iter() {
             let mut rng = rand::thread_rng();
-
-            let mut grid = grid_query.single_mut();
-            let params = params_query.single();
 
             let particle_width = window.width() / params.grid_size as f32;
             let particle_height = window.height() / params.grid_size as f32;
 
-            for (mut p, mut coord, mut transform) in particles_query.iter_mut() {
+            for (_, mut coord, mut transform) in particles.iter_mut() {
                 let next_dir: Dir = rng.gen();
                 let dir_coord: Coord<i8> = next_dir.value();
                 let next_coord = Coord {
@@ -193,15 +191,16 @@ fn sim_step(
                 }
 
                 //collision check
-                if *coord != next_coord && grid.data[next_coord.x as usize][next_coord.y as usize] {
+                if *coord != next_coord && grid.data[[next_coord.x as usize, next_coord.y as usize]]
+                {
                     continue;
                 }
 
                 transform.translation.x = next_coord.x as f32 * particle_width;
                 transform.translation.y = next_coord.y as f32 * particle_height;
 
-                grid.data[coord.x as usize][coord.y as usize] = false;
-                grid.data[next_coord.x as usize][next_coord.y as usize] = true;
+                grid.data[[coord.x as usize, coord.y as usize]] = false;
+                grid.data[[next_coord.x as usize, next_coord.y as usize]] = true;
 
                 *coord = next_coord;
             }
@@ -218,10 +217,116 @@ fn center_camera(mut windows: ResMut<Windows>, mut camera: Query<&mut Transform,
     }
 }
 
-#[inline]
-fn position_to_coord(pos: (f32, f32), particle_width: f32, particle_height: f32) -> Coord<usize> {
-    Coord {
-        x: (pos.0 / particle_width) as usize,
-        y: (pos.1 / particle_height) as usize,
+fn reset_sim(
+    windows: Res<Windows>,
+    params: Res<Parameters>,
+    mut sim_state: ResMut<SimState>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    particles: Query<(Entity, &Particle)>,
+) {
+    if sim_state.reset {
+        for window in windows.iter() {
+            for e in particles.iter() {
+                commands.entity(e.0).despawn_recursive();
+            }
+            commands.remove_resource::<Grid>();
+
+            let particle_width = window.width() / params.grid_size as f32;
+            let particle_height = window.height() / params.grid_size as f32;
+
+            let particle_size = 1.5 * particle_height;
+
+            let (particles, grid) = get_particles(params.n_particles, params.grid_size);
+            for p in particles {
+                commands
+                    .spawn()
+                    .insert(Particle)
+                    .insert(Coord::<isize> {
+                        x: p.x as isize,
+                        y: p.y as isize,
+                    })
+                    .insert_bundle(MaterialMesh2dBundle {
+                        mesh: meshes
+                            .add(
+                                shape::Quad::new(Vec2 {
+                                    x: particle_size,
+                                    y: particle_size,
+                                })
+                                .into(),
+                            )
+                            .into(),
+                        material: materials.add(ColorMaterial::from(Color::WHITE)),
+                        transform: Transform::from_translation(Vec3::new(
+                            (p.x as f32) * particle_width,
+                            (p.y as f32) * particle_height,
+                            0.,
+                        )),
+                        ..default()
+                    });
+            }
+
+            commands.insert_resource(grid);
+
+            sim_state.reset = false;
+        }
     }
+}
+
+fn handle_input(
+    keys: Res<Input<KeyCode>>,
+    mut timer: ResMut<SimTimer>,
+    mut sim_state: ResMut<SimState>,
+) {
+    if keys.just_pressed(KeyCode::Space) {
+        sim_state.paused ^= true;
+    }
+    if keys.just_pressed(KeyCode::R) {
+        sim_state.reset = true;
+    }
+
+    //sim speed control
+    if keys.just_pressed(KeyCode::Key1) {
+        timer.0.set_duration(Duration::from_secs_f32(0.1));
+    }
+    if keys.just_pressed(KeyCode::Key2) {
+        timer.0.set_duration(Duration::from_secs_f32(0.05));
+    }
+    if keys.just_pressed(KeyCode::Key3) {
+        timer.0.set_duration(Duration::from_secs_f32(0.025));
+    }
+}
+
+fn get_particles(n_particles: usize, grid_size: usize) -> (Vec<Coord<isize>>, Grid) {
+    let mut res = Vec::<Coord<isize>>::new();
+    res.reserve_exact(n_particles);
+
+    let v = vec![false; grid_size * grid_size];
+    let mut grid = Grid {
+        data: Array2::<bool>::from_shape_vec((grid_size, grid_size), v).unwrap(),
+    };
+
+    let mut rng = rand::thread_rng();
+
+    let mut n = 0;
+    while n < n_particles {
+        let x = rng.gen_range(0..grid_size);
+        let y = rng.gen_range(0..grid_size);
+
+        if grid.data[[x, y]] {
+            continue;
+        }
+
+        grid.data[[x, y]] = true;
+
+        let coord = Coord::<isize> {
+            x: x as isize,
+            y: y as isize,
+        };
+        res.push(coord);
+
+        n += 1;
+    }
+    (res, grid)
 }
